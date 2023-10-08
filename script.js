01ac9f47bb08a73e1d98e3e00eb03c34ce77aca0
Aman Beg <am2835112@gmail.com>
2023-10-08 21:31:27 +0530

    api fixing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const API_KEY = "fbd8c3bc2c574bb0aad5287e49594be8";
+// const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "fbd8c3bc2c574bb0aad5287e49594be8";  // mykey
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener('load',()=> fetchNews("India"));
